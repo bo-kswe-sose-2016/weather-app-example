04ec2d46e706ea096129a99d25e07dc52ca126a6
matthesrieke <m.rieke@52north.org>
2016-04-15 12:49:36 +0200

    initial commit

---
+++ src/main/java/de/matthesrieke/weather/WeatherServlet.java
@@ -0,0 +1,24 @@
+package de.matthesrieke.weather;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class WeatherServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		double temp = 0.0;
+		//TODO use OWM
+		resp.getWriter().append("Temperatur: "+ temp);
+	}
+	
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Highly sophistiacted weather API</h2>
+</body>
+</html>
